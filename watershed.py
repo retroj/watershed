ed58a02ca400e34c4399e415b6e1771285fc5462
John Foerch <jjfoerch@gmail.com>
2018-07-20 22:56:50 -0400

    watershed: initial support for dotstar led strip and pollution

--- watershed.py
+++ watershed.py
@@ -4,15 +4,54 @@ from time import sleep, time
 from random import random, randint
 from samplebase import SampleBase
 from PIL import Image, ImageDraw
+from dotstar import Adafruit_DotStar
+
+
+class LEDStripSection ():
+    offset = 0
+    length = 0
+    direction = 1
+
+    def __init__ (self, owner, offset, length, direction):
+        self.owner = owner
+        self.offset = offset
+        self.length = length
+        self.direction = direction
+
+    def set_pixel (self, i, color):
+        self.owner.strip.setPixelColor(self.offset+i, color)
+
+
+
+class LEDStrip ():
+    sections = None
+    npixels = 0
+    strip = None
+    def __init__ (self, datapin=24, clockpin=25, sections=[]):
+        self.sections = {}
+        for i,spec in enumerate(sections):
+            self.add_section(**spec)
+        self.strip = Adafruit_DotStar(self.npixels, datapin, clockpin)
+        self.strip.begin()
+        self.strip.setBrightness(64)
+
+    def add_section (self, name = None, offset = None, length = 0, direction = 1):
+        if offset is None:
+            offset = self.npixels
+        self.sections[name] = LEDStripSection(self, offset, length, direction)
+        self.npixels = max(self.npixels, offset + length)
 
 
 class Sprites ():
     assets = {}
 
     @staticmethod
-    def get (name):
+    def get (name, generate=None):
         if not name in Sprites.assets:
-            img = Image.open(name)
+            if generate:
+                img = generate()
+            else:
+                img = Image.open(name)
             r, g, b, a = img.split()
             sprite = Image.merge("RGB", (r, g, b))
             mask = Image.merge("L", (a,))
@@ -28,18 +67,62 @@ class Mob ():
     mask = None
     width = 0
     height = 0
+    x = 0
+    y = 0
 
     def __init__ (self, pond, t):
         self.spawn_time = t
 
-    def update (self, pond):
+    def update (self, pond, t):
         return True
+
+
+class Pollution (Mob):
+    last_spawn = time()
+    strip = None
+
+    def __init__ (self, pond, t):
+        super(Pollution, self).__init__(pond, t)
+        print("spawning pollution "+str(int(t)))
+        self.sprite, self.mask, self.width, self.height = \
+            Sprites.get("Pollution", Pollution.draw_sprite)
+        self.strip = pond.ledstrip.sections["bad"]
+        self.y = -(self.strip.length)
+        self.x = 40
+
+    @staticmethod
+    def draw_sprite ():
+        img = Image.new("RGBA", (3, 3))
+        draw = ImageDraw.Draw(img)
+        draw.ellipse((0,0,2,2), fill=(255,0,0))
+        return img
+
+
+    ## Public Interface
+    ##
+    @staticmethod
+    def maybe_spawn (pond, t):
+        if t > Pollution.last_spawn + 10:
+            Pollution.last_spawn = t
+            return Pollution(pond, t)
+
+    def update (self, pond, t):
+        if t > self.spawn_time + 9:
+            print("despawning pollution")
+            return False
+        if self.y < 0: # strip
+            self.strip.set_pixel(abs(self.y) + 1, 0x000000)
+            self.strip.set_pixel(abs(self.y), 0x00FF00)
+            self.y += 1
+        else:          # matrix
+            pond.canvas.paste(self.sprite, (32, 16), self.mask)
+        return True
+
+
 
 
 class Fish (Mob):
     start_x = 0
-    x = 0
-    y = 0
     direction = 0
     speed = 0
 
@@ -48,15 +131,24 @@ class Fish (Mob):
         self.direction = 1 if random() < 0.5 else -1
         self.speed = random() * 6 + 2
         if self.direction == 1:
-            self.sprite, self.mask, self.width, self.height =
+            self.sprite, self.mask, self.width, self.height = \
                 Sprites.get("assets/sprites/Fish1/Fish1-right.png")
         else:
-            self.sprite, self.mask, self.width, self.height =
+            self.sprite, self.mask, self.width, self.height = \
                 Sprites.get("assets/sprites/Fish1/Fish1-left.png")
         self.start_x = -self.width if self.direction == 1 else pond.width
         ##XXX: this math can allow the lower level to be above the upper level,
         ##     resulting in an error
         self.y = randint(pond.level_px + self.height * 0.5, pond.height - self.height * 1.5)
+
+
+    ## Public Interface
+    ##
+    @staticmethod
+    def maybe_spawn (pond, t):
+        if pond.level > 0.5 and pond.health > 0.5 and random() < 0.001:
+            print("spawning fish")
+            return Fish(pond, t)
 
     def update (self, pond, t):
         self.x = int(self.start_x + self.speed * self.direction * (t - self.spawn_time))
@@ -69,16 +161,9 @@ class Fish (Mob):
         pond.canvas.paste(self.sprite, (self.x, self.y), self.mask)
         return True
 
-    @staticmethod
-    def maybe_spawn (pond, t):
-        if pond.level > 0.5 and pond.health > 0.5 and random() < 0.001:
-            print("spawning fish")
-            return Fish(pond, t)
-
-
-
 
 class Pond (SampleBase):
+    ledstrip = None
     width = 0
     height = 0
     canvas = None
@@ -90,6 +175,13 @@ class Pond (SampleBase):
     def __init__ (self, *args, **kwargs):
         super(Pond, self).__init__(*args, **kwargs)
         self.mobs = []
+        self.ledstrip = LEDStrip(
+            datapin=24, clockpin=25,
+            sections=[{ "name": "wave", "length": 37, "direction": -1 },
+                      { "name": "rain", "length": 37, "direction": -1 },
+                      { "name": "good", "length": 37, "direction": 1 },
+                      { "name": "bad", "length": 37, "direction": -1 }])
+
 
     def adjust_level (self):
         l = self.level
@@ -101,9 +193,10 @@ class Pond (SampleBase):
             self.level_px = int(self.level * -32 + 32)
 
     def spawn_mobs (self, t):
-        f = Fish.maybe_spawn(self, t)
-        if f:
-            self.mobs.append(f)
+        for x in [Fish, Pollution]:
+            f = x.maybe_spawn(self, t)
+            if f:
+                self.mobs.append(f)
 
     def draw_bg (self):
         """draw the pond onto self.canvas up to the level represented by self.level"""
@@ -139,6 +232,10 @@ class Pond (SampleBase):
             ##
             double_buffer.SetImage(self.canvas)
             double_buffer = self.matrix.SwapOnVSync(double_buffer)
+
+            ## write led strip
+            ##
+            self.ledstrip.strip.show()
 
             sleep(0.01)
 
