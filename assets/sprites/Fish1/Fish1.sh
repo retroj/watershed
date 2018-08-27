36940f0a1fd22819e643b3b0f2cdc5b5dbf2bd30
John Foerch <jjfoerch@gmail.com>
2018-08-26 23:01:23 -0400

    Fish1: make it a bit darker

--- assets/sprites/Fish1/Fish1.sh
+++ assets/sprites/Fish1/Fish1.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-convert -density 3 -background none Fish1.svg -write Fish1-left.png -flop Fish1-right.png
+convert -density 3 -background none Fish1.svg \
+        -evaluate multiply 0.4 \
+        -write Fish1-left.png -flop Fish1-right.png
