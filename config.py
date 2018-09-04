0cff5b7a722c9244187d8433268473486c5c036e
John Foerch <jjfoerch@gmail.com>
2018-09-03 21:55:31 -0400

    config: switch to full ledstrips

--- config.py
+++ config.py
@@ -5,24 +5,24 @@ LEDStrip.datapin = 24
 LEDStrip.clockpin = 25
 
 ## 5m test setup
-LEDStrip.sections_spec = [
-    { "name": "wave", "length": 37, "direction": -1 },
-    { "name": "rain", "length": 37, "direction": -1,
-      "voffset": -1, "vmul": -1 },
-    { "name": "gooddroplet", "length": 37, "direction": 1,
-      "voffset": -1, "vmul": -1 },
-    { "name": "baddroplet", "length": 37, "direction": -1,
-      "voffset": -1, "vmul": -1 }]
+# LEDStrip.sections_spec = [
+#     { "name": "wave", "length": 37, "direction": -1 },
+#     { "name": "rain", "length": 37, "direction": -1,
+#       "voffset": -1, "vmul": -1 },
+#     { "name": "gooddroplet", "length": 37, "direction": 1,
+#       "voffset": -1, "vmul": -1 },
+#     { "name": "baddroplet", "length": 37, "direction": -1,
+#       "voffset": -1, "vmul": -1 }]
 
 ## full deployment setup
-# LEDStrip.sections_spec = [
-#     { "name": "wave", "length": 150, "direction": -1 },
-#     { "name": "rain", "length": 121, "direction": -1,
-#       "voffset": -1, "vmul": -1 },
-#     { "name": "gooddroplet", "length": 65, "direction": 1,
-#       "voffset": -1, "vmul": -1 },
-#     { "name": "baddroplet", "length": 66, "direction": -1,
-#       "voffset": -1, "vmul": -1 }]
+LEDStrip.sections_spec = [
+    { "name": "wave", "length": 150, "direction": -1 },
+    { "name": "rain", "length": 121, "direction": -1,
+      "voffset": -1, "vmul": -1 },
+    { "name": "gooddroplet", "length": 65, "direction": 1,
+      "voffset": -1, "vmul": -1 },
+    { "name": "baddroplet", "length": 66, "direction": -1,
+      "voffset": -1, "vmul": -1 }]
 
 Pond.healthsteps = 12
 Pond.active_spawners = [Fish, Rain]
@@ -30,20 +30,20 @@ Pond.initial_level = 0.7
 
 Rain.length = 7
 Rain.color = (0, 0, 0xff)
-Rain.start_x = 10
+Rain.start_x = 0
 Rain.airspeed = (1.5, 8)
 Rain.waterspeed = (1.5, 8)
 Rain.fadetime = 3.5
 
 GoodDroplet.length = 7
 GoodDroplet.color = (0, 0, 0xff)
-GoodDroplet.start_x = 30
+GoodDroplet.start_x = 20
 GoodDroplet.airspeed = (1.5, 8)
 GoodDroplet.waterspeed = (0, 5)
 
 BadDroplet.length = 7
 BadDroplet.color = (0xaa, 0, 0)
-BadDroplet.start_x = 40
+BadDroplet.start_x = 48
 BadDroplet.airspeed = (-1.5, 8)
 BadDroplet.waterspeed = (0, 5)
 
